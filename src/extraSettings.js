d4b82a90e1b4fd6c33b48113d0bcfae4db8eeb38
YukiZH <78311595+YukiZH@users.noreply.github.com>
2021-02-01 11:23:38 +0800

    Add files via upload

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -3,17 +3,17 @@
  */
 
 // 额外分数：修改数字
-let extraScore = 1;
+let extraScore = 20;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10
 let firstFruit = 0;
 
 // 指定生成的水果
 const minRandomFruitNum = 0; // 生成随机水果最小值（0-10）
-const maxRandomFruitNum = 5; // 生成随机水果最大值（1-11）
+const maxRandomFruitNum = 6; // 生成随机水果最大值（1-11）
 let setFruits = {
   startFruits: [0, 0, 1, 2, 2, 3], // 指定前几次生成的水果，可填入任意数量的数字
   randomFunction: () => {
@@ -22,19 +22,19 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png';
+let adLink = false;
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成蓝色生死恋';
 
 // 开启选分弹窗：将 false 改为 true
-let selectModal = false;
+let selectModal = true;
 
 // 水果合成反转：false 改为 true（尚未支持）
 let reverseLevelUp = false;
